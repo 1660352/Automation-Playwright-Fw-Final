f70ab5bbb3507c81cf8322066c667f9f259a64fb
Tue Tran <tue.tranduy@nashtechglobal.com>
2026-07-18 17:04:40 +0700

    Enhance login test to include URL expectation and remove unnecessary timeout

--- tests/login-tests.spec.ts
+++ tests/login-tests.spec.ts
@@ -1,7 +1,8 @@
-import test from '@core/fixtures/all.fixture';
+import test, { expect } from '@core/fixtures/all.fixture';
 
 test('ADMIN - Login successfully with valid username and password', async ({ page, loginPage }) => {
     await loginPage.openUrl();
     await loginPage.doLogin('admin', 'password123');
-    await page.waitForTimeout(2000);
+
+    await expect(page).toHaveURL('/home');
 });

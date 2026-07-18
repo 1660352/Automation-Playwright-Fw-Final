7c5010a2fd89c69c1aaa622f37599517707ef738
Tue Tran <tue.tranduy@nashtechglobal.com>
2026-07-18 17:48:26 +0700

    update

--- tests/login-tests.spec.ts
+++ tests/login-tests.spec.ts
@@ -1,5 +1,4 @@
 import test, { expect } from '@core/fixtures/all.fixture';
-import { ReportUtils } from '@core/utils/report-utils';
 import * as allure from "allure-js-commons";
 
 test('ADMIN - Login successfully with valid username and password', async ({ page, loginPage }) => {
@@ -13,8 +12,6 @@ test('ADMIN - Login successfully with valid username and password', async ({ pag
     });
 
     await allure.step('Step 3: Verify successful login', async () => {
-        await ReportUtils.attachScreenshot("should see home page", page, async () => {
-            await expect(page).toHaveURL('/home');
-        });
+        await expect(page).toHaveURL('/home');
     });
 });

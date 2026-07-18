8d8e82d851fe2861a972c98906bb549ba7cd667f
Tue Tran <tue.tranduy@nashtechglobal.com>
2026-07-18 17:48:12 +0700

    feat: integrate Allure reporting and enhance login test steps
    - Added Allure reporting scripts to package.json for generating HTML reports.
    - Updated Playwright configuration to use Allure as a reporter alongside the default HTML reporter.
    - Refactored login test to include detailed steps using Allure's step functionality for better reporting.
    - Created Allure configuration file (allurerc.mjs) to customize report generation settings.
    - Added history.jsonl to track test results for Allure reporting.

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { outputFolder: './reports/playwright-report' }], ["allure-playwright", { resultsDir: './reports/allure-results' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'https://shopdemo-e3gwg9hqaygghmbv.canadacentral-01.azurewebsites.net',
--- tests/login-tests.spec.ts
+++ tests/login-tests.spec.ts
@@ -1,8 +1,20 @@
 import test, { expect } from '@core/fixtures/all.fixture';
+import { ReportUtils } from '@core/utils/report-utils';
+import * as allure from "allure-js-commons";
 
 test('ADMIN - Login successfully with valid username and password', async ({ page, loginPage }) => {
-    await loginPage.openUrl();
-    await loginPage.doLogin('admin', 'password123');
 
-    await expect(page).toHaveURL('/home');
+    await allure.step('Step 1: Navigate to the login page', async () => {
+        await loginPage.openUrl();
+    });
+
+    await allure.step('Step 2: Enter valid username and password', async () => {
+        await loginPage.doLogin('admin', 'password123');
+    });
+
+    await allure.step('Step 3: Verify successful login', async () => {
+        await ReportUtils.attachScreenshot("should see home page", page, async () => {
+            await expect(page).toHaveURL('/home');
+        });
+    });
 });

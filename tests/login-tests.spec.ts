7e4d068b7586d35aefa33c774057d6c0d829c78c
Tue Tran <tue.tranduy@nashtechglobal.com>
2026-07-22 14:50:55 +0700

    feat: refactor login tests for Admin and Customer roles to improve structure and readability

--- tests/login-tests.spec.ts
+++ tests/login-tests.spec.ts
@@ -3,92 +3,76 @@ import { ReportUtils } from '@core/utils/report-utils';
 import * as allure from "allure-js-commons";
 import accounts from '../resources/accounts.json';
 
-test('ADMIN - Login successfully with valid username and password', async ({ page, loginPage }) => {
-
+test.describe('Login Tests - Admin Role', () => {
     var account = accounts.find(account => account.role === 'admin') || { username: '', password: '' };
 
-    await allure.step('Step 1: Navigate to the login page', async () => {
-        await loginPage.openUrl();
+    test.beforeEach('Navigate to Login Page and perform login', async ({ loginPage }) => {
+        await allure.step('Step 1: Navigate to the login page', async () => {
+            await loginPage.openUrl();
+        });
+
+        await allure.step('Step 2: Enter valid username and password', async () => {
+            await loginPage.doLogin(account.username, account.password);
+        });
     });
 
-    await allure.step('Step 2: Enter valid username and password', async () => {
-        await loginPage.doLogin(account.username, account.password);
+    test('ADMIN - Login successfully with valid username and password', async ({ page }) => {
+        await allure.step('Verify successful login', async () => {
+            await ReportUtils.attachScreenshot("should see home page", page, async () => {
+                await expect(page).toHaveURL('/home');
+            });
+        });
     });
 
-    await allure.step('Step 3: Verify successful login', async () => {
-        await ReportUtils.attachScreenshot("should see home page", page, async () => {
-            await expect(page).toHaveURL('/home');
+    test('ADMIN - Verify products are displayed in Featured Products section', async ({ page, homePage }) => {
+        await allure.step('Verify products are displayed in Featured Products section', async () => {
+            await ReportUtils.attachScreenshot("should see featured products", page, async () => {
+                await expect(async () => {
+                    const productCardsCount = await homePage.getProductCardsCount();
+                    expect(productCardsCount).toBeGreaterThan(0);
+                }).toPass();
+            });
         });
     });
 });
 
-test('ADMIN - Verify products are displayed in Featured Products section', async ({ page, loginPage, homePage }) => {
-
-    var account = accounts.find(account => account.role === 'admin') || { username: '', password: '' };
-
-    await allure.step('Step 1: Navigate to the login page', async () => {
-        await loginPage.openUrl();
-    });
-
-    await allure.step('Step 2: Enter valid username and password', async () => {
-        await loginPage.doLogin(account.username, account.password);
-    });
-
-    await allure.step('Step 4: Verify products are displayed in Featured Products section', async () => {
-        await ReportUtils.attachScreenshot("should see featured products", page, async () => {
-            await expect(async () => {
-                const productCardsCount = await homePage.getProductCardsCount();
-                expect(productCardsCount).toBeGreaterThan(0);
-            }).toPass();
-        });
-    });
-});
-
-test('USER - Login successfully with valid username and password', async ({ page, loginPage }) => {
-
+test.describe('Login Tests - Customer Role', () => {
     var account = accounts.find(account => account.role === 'customer') || { username: '', password: '' };
 
-    await allure.step('Step 1: Navigate to the login page', async () => {
-        await loginPage.openUrl();
-    });
-
-    await allure.step('Step 2: Enter valid username and password', async () => {
-        await loginPage.doLogin(account.username, account.password);
-    });
-
-    await allure.step('Step 3: Verify successful login', async () => {
-        await ReportUtils.attachScreenshot("should see home page", page, async () => {
-            await expect(page).toHaveURL('/home');
+    test.beforeEach('Navigate to Login Page and perform login', async ({ loginPage }) => {
+        await allure.step('Step 1: Navigate to the login page', async () => {
+            await loginPage.openUrl();
         });
-    });
-});
 
-test('USER - User able to view user’s profile successfully', async ({ page, loginPage, profilePage, homePage }) => {
-
-    var account = accounts.find(account => account.role === 'customer') || { username: '', password: '' };
-
-    await allure.step('Step 1: Navigate to the login page', async () => {
-        await loginPage.openUrl();
-    });
-
-    await allure.step('Step 2: Enter valid username and password', async () => {
-        await loginPage.doLogin(account.username, account.password);
-    });
-
-    await allure.step('Step 3: Go to user profile page', async () => {
-        await homePage.clickOnHeaderUsername();
-    });
-
-    await allure.step('Step 4: Verify user profile page is displayed', async () => {
-        await ReportUtils.attachScreenshot("should see profile page", page, async () => {
-            await expect(page).toHaveURL('/profile');
+        await allure.step('Step 2: Enter valid username and password', async () => {
+            await loginPage.doLogin(account.username, account.password);
         });
     });
 
-    await allure.step('Step 5: Verify user profile information is displayed correctly', async () => {
-        await ReportUtils.attachScreenshot("should see profile information", page, async () => {
-            const usernameInputValue = await profilePage.getUserNameInputValue();
-            expect(usernameInputValue).toBe('ate.user1');
+    test('Customer - Login successfully with valid username and password', async ({ page }) => {
+        await allure.step('Verify successful login', async () => {
+            await ReportUtils.attachScreenshot("should see home page", page, async () => {
+                await expect(page).toHaveURL('/home');
+            });
+        });
+    });
+
+    test('Customer - User able to view user’s profile successfully', async ({ page, profilePage, homePage }) => {
+        await allure.step('Step 1: Go to user profile page', async () => {
+            await homePage.clickOnHeaderUsername();
+        });
+
+        await allure.step('Step 2: Verify user profile page is displayed', async () => {
+            await ReportUtils.attachScreenshot("should see profile page", page, async () => {
+                await expect(page).toHaveURL('/profile');
+            });
+        });
+
+        await allure.step('Step 3: Verify user profile information is displayed correctly', async () => {
+            await ReportUtils.attachScreenshot("should see profile information", page, async () => {
+                const usernameInputValue = await profilePage.getUserNameInputValue();
+                expect(usernameInputValue).toBe('ate.user1');
+            });
         });
     });
 });
